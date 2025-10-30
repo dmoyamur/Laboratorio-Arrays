077ad0af5de449e8f4ee096480ab3970e8e235f5
dmoyamur <dmoya844@gmail.com>
2025-10-30 15:11:41 -0500

    Avance hasta el ejercicio 4

---
+++ Laboratorio.js
@@ -0,0 +1,57 @@
+const prompt = require("prompt-sync")();
+
+console.log(" Ejercicio 1. Calculadora de Propinas");
+function calcularPropina(total, porcentaje){
+    propinac = total + (total * porcentaje);
+    return propinac;
+};
+total = Number(prompt("Ingrese el total de la factura: "));
+propina= Number(prompt("Ingrese el porcentaje de la propina: "));
+valoraPagar = calcularPropina(total,propina);
+console.log("El valor total a pagar es de: "+ valoraPagar);
+
+
+console.log("\n Ejercicio 2. Validar Contraseña");
+function validarContraseña(contraseña){
+    let validacion = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
+    return validacion.test(contraseña)
+};
+let contraseña = prompt("Ingrese una contraseña: ");
+while (!validarContraseña(contraseña)){
+    console.log("Contraseña no valida, la contraseña debe tener minimo 8 caracteres, un número y una mayúscula.");
+    contraseña = prompt("Intente nuevamente: ");
+};
+console.log("Contraseña válida");
+
+console.log("\n Ejercicio 3. Buscar Producto en Inventario")
+function buscarProducto(nombre,inventario){
+    let producto = inventario.find(item=>item.nombre.toLowerCase()=== nombre.toLowerCase())
+    return producto || null
+};
+const inventario = [
+    {nombre:"zapatos", saldo:20},
+    {nombre:"medias", saldo:15},
+    {nombre:"camisa", saldo:12},
+    {nombre:"camiseta", saldo:2},
+];
+let buscar=prompt("Ingrese un producto: ");
+let datosProducto = buscarProducto(buscar,inventario);
+console.log(datosProducto);
+
+console.log("\n Ejercicio 4. Calcular Promedio de Notas")
+function calcularPromedio(notas){
+    let suma = 0;
+    for (let i =0; i<notas.length; i++){
+        suma += notas[i]
+    }
+    let promedio = suma/notas.length
+    return promedio
+}
+let cantNotas = Number(prompt("Cuántas notas desea ingresar?: "));
+let notas =[];
+for (let i=0; i<cantNotas; i++){
+    let nota = Number(prompt("Ingrese la nota "+(i+1)+" :"));
+    notas.push(nota)
+}
+let promedio = calcularPromedio(notas);
+console.log("El promedio de las notas es: "+ promedio);
